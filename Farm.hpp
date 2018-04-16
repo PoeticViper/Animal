309d47cac9929377856219496e914f322345c546
PoeticViper <37876024+PoeticViper@users.noreply.github.com>
2018-04-16 09:07:27 -0500

    Operator '>>' now works

--- Farm.hpp
+++ Farm.hpp
@@ -9,31 +9,30 @@ class Farm {
 	
 		private:
 			 std::vector<Animal*>animals;
-			 Farm* _farm = this;
-			 Animal* convertToAnimal(std::istream*& input)
+			 Animal* convertToAnimal(std::istream& input)
 			 {
-			 	std::istream *temp = *&input;
+			 	std::istream *temp = &input;
 			 	return dynamic_cast<Animal*>(temp);
 			 }
-			
 		public:
 			Animal* getAnimal(int index)
 			{
 				return animals.at(index);
 			}
-		friend Farm* operator>>(Farm* farm ,  Animal* animal)
-		{
-			farm.animals.push_back(animal);
-			return farm;
-		}
 
 		~Farm() {
 			std::cout << "The farm has burned down! Major oof" << std::endl;	
 		}
 		
+		friend Animal* operator>>(Animal *input,Farm &f)
+		{
+    		f.animals.push_back(input);
+    		return input;
+		}
+		
 		friend Farm *operator*(Farm obj)
 	{
-		return _farm;
+		return *obj;
 }
 };
 
